931d973fd778dd0910e5e2ed898bee4373ceea17
Scott Ransom <sransom@nrao.edu>
2008-08-28 22:56:02 -0400

    Big cleanups and serious speed-ups of the downsampling code. It has been tested quite a bit and it seems to work fine in real time.
    Also added a function that sets all of the zeroth and nchan-1'th channels
    explicitly to zero (along with the weights) to get rid of the FFT 
    contamination present in those channels.

--- downsample.c
+++ downsample.c
@@ -10,7 +10,7 @@ void get_stokes_I(struct psrfits *pf)
 /* Move the Stokes I in place so that it is consecutive in the array */
 {
     int ii, inbytes, outbytes;
-    struct hdrinfo *hdr = &(pf->hdr); // dereference the ptr to the header struct
+    struct hdrinfo *hdr = &(pf->hdr);
     unsigned char *data = pf->sub.data;
 
     outbytes = hdr->nbits * hdr->nchan / 8;
@@ -24,22 +24,29 @@ void downsample_freq(struct psrfits *pf)
 /* Average adjacent frequency channels together in place    */
 /* Note:  this only works properly for 8-bit data currently */
 {
-    int ii, jj, itmp, iidx=0, oidx=0;
-    struct hdrinfo *hdr = &(pf->hdr); // dereference the ptr to the header struct
-    unsigned char *data = pf->sub.data;
+    int ii, jj, itmp;
+    struct hdrinfo *hdr = &(pf->hdr);
+    char *indata = (char *)pf->sub.data;
+    char *outdata = (char *)pf->sub.data;
     const int dsfact = hdr->ds_freq_fact;
-    float norm = 1.0 / dsfact;
+    const int offset = dsfact >> 1;
+    const int shift = ffs(dsfact) - 1; // dsfact's power of 2
 
     // Treat the polns as being parts of the same spectrum
     int out_npol = hdr->npol;
     if (hdr->onlyI) out_npol = 1;
     const int out_nchan = hdr->nchan * out_npol / hdr->ds_freq_fact;
     
-    for (ii = 0 ; ii < hdr->nsblk * out_nchan ; ii++) { // Iterate over the times and chans
-        itmp = 0;
-        for (jj = 0 ; jj < dsfact ; jj++) // ... adjacent chans
-            itmp += (char)data[oidx++]; 
-        data[iidx++] = (int) rintf(((float) itmp) * norm);
+    // Iterate over the times and output chans
+    for (ii = 0 ; ii < hdr->nsblk * out_nchan ; ii++) {
+        // and over adjacent input chans for each time
+        for (jj = 0, itmp = offset ; jj < dsfact ; jj++)
+            itmp += *indata++;
+        // The following adds 1/2 of dsfact to the total (which allows
+        // for rounding-type behavior) and then divides by dsfact (via
+        // a bit shift of the appropriate number of bits, since dsfact
+        // should be a power-of-two).
+        *outdata++ = itmp >> shift;
     }
 }
 
@@ -48,28 +55,34 @@ void downsample_time(struct psrfits *pf)
 /* This should be called _after_ downsample_freq() */
 /* Note:  this only works properly for 8-bit data currently */
 {
-    int ii, jj, kk, itmp, iidx, oidx;
-    struct hdrinfo *hdr = &(pf->hdr); // dereference the ptr to the header struct
-    unsigned char *data = pf->sub.data;
-    int dsfact = hdr->ds_time_fact;
-    float norm = 1.0 / dsfact;
+    int ii, jj, kk, itmp, chanoff1, chanoff2;
+    struct hdrinfo *hdr = &(pf->hdr);
+    char *data = (char *)pf->sub.data;
+    char *indata, *outdata;
+    const int dsfact = hdr->ds_time_fact;
+    const int offset = dsfact >> 1;
+    const int shift = ffs(dsfact) - 1; // dsfact's power of 2
 
     // Treat the polns as being parts of the same spectrum
     int out_npol = hdr->npol;
     if (hdr->onlyI) out_npol = 1;
-    int out_nchan = hdr->nchan * out_npol / hdr->ds_freq_fact;
-    int out_nsblk = hdr->nsblk / dsfact;
+    const int out_nchan = hdr->nchan * out_npol / hdr->ds_freq_fact;
+    const int out_nsblk = hdr->nsblk / dsfact;
     
-    for (ii = 0 ; ii < out_nsblk ; ii++) { // Iterate over the output time-slices
-        iidx = ii * out_nchan;
-        for (jj = 0 ; jj < out_nchan ; jj++) { // ... channels
-            itmp = 0;
-            oidx = ii * dsfact * out_nchan + jj;
-            for (kk = 0 ; kk < dsfact ; kk++) { // ... adjacent times
-                itmp += (char)data[oidx];
-                oidx += out_nchan;
+    // Iterate over the output times
+    for (ii = 0 ; ii < out_nsblk ; ii++) {
+        chanoff1 = ii * out_nchan;
+        chanoff2 = chanoff1 * dsfact;
+        outdata = data + chanoff1;
+        // and over each channel
+        for (jj = 0 ; jj < out_nchan ; jj++) {
+            indata = data + chanoff2 + jj;
+            // to add the adjacent times
+            for (kk = 0, itmp = offset ; kk < dsfact ; kk++) {
+                itmp += *indata;
+                indata += out_nchan;
             }
-            data[iidx+jj] = (int) rintf((float) itmp * norm);
+            *outdata++ = itmp >> shift;
         }
     }
 }
@@ -78,8 +91,8 @@ void guppi_update_ds_params(struct psrfits *pf)
 /* Update the various output data arrays / values so that */
 /* they are correct for the downsampled data.             */
 {
-    struct hdrinfo *hdr = &(pf->hdr); // dereference the ptr to the header struct
-    struct subint  *sub = &(pf->sub); // dereference the ptr to the subint struct
+    struct hdrinfo *hdr = &(pf->hdr);
+    struct subint  *sub = &(pf->sub);
 
     int out_npol = hdr->npol;
     if (hdr->onlyI) out_npol = 1;
