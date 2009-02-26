77b95f3103d12c2c334742b70eda713a274349c7
Paul Demorest <pdemores@nrao.edu>
2009-02-25 21:07:30 -0500

    Fixed polyco selection segfault. Added more verbose polyco read statement.

--- polyco.c
+++ polyco.c
@@ -107,7 +107,8 @@ int select_pc(const struct polyco *pc, int npc, const char *psr,
         int imjd, double fmjd) {
     int ipc;
     const char *tmp = psr;
-    if (tmp[0]=='J' || tmp[0]=='B') tmp++;
+    if (psr!=NULL)
+        if (tmp[0]=='J' || tmp[0]=='B') tmp++;
     // Verbose
     //fprintf(stderr, "Looking for polycos with src='%s' imjd=%d fmjd=%f\n",
     //        tmp, imjd, fmjd);
@@ -309,10 +310,11 @@ int make_polycos(const char *parfile, struct hdrinfo *hdr,
     fclose(fout);
 
     /* Call tempo */
-    double mjd0, mjd1;
-    mjd0 = (double)hdr->MJD_epoch - 0.5;
-    mjd1 = (double)(hdr->MJD_epoch + hdr->scanlen/86400.0 + 0.5);
-    sprintf(line, "echo %.3f %.3f | tempo -z -f pulsar.par > /dev/null",
+    int mjd0, mjd1;
+    mjd0 = (int)hdr->MJD_epoch;
+    //mjd1 = mjd0+1;
+    mjd1 = (int)(hdr->MJD_epoch + hdr->scanlen/86400.0 + 0.5);
+    sprintf(line, "echo %d %d | tempo -z -f pulsar.par > /dev/null",
             mjd0, mjd1);
     system(line);
 
