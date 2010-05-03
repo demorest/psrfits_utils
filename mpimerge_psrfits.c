deb1a785a05d2da63141e0b52145a9dee6d74a0b
Scott Ransom <sransom@nrao.edu>
2010-05-03 19:16:48 -0400

    Initial (likely) working version.

--- mpimerge_psrfits.c
+++ mpimerge_psrfits.c
@@ -8,18 +8,34 @@
 
 int main(int argc, char *argv[])
 {
-    int ii, jj, status, statsum;
-    int numprocs, numbands, myid, gpuid, *gpuids;
+    int ii, nc, ncnp, status=0, statsum=0;
+    int numprocs, numbands, myid;
+    int *counts, *offsets;
     struct psrfits pf;
     char hostname[100];
     MPI_Status mpistat;
-    MPI_Group all_group, gpu_group; 
-    MPI_Comm gpu_comm; 
 
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
     MPI_Comm_rank(MPI_COMM_WORLD, &myid);
     numbands = numprocs - 1;
+
+    // Call usage() if we have no command line arguments
+    if (argc == 1) {
+        if (myid == 0) {
+            printf("usage:  mpimerge_psrfits basefilename\n\n");
+        }
+        MPI_Finalize();
+        exit(1);
+    }
+    
+    if (myid == 0) { // Master proc only
+        printf("\n\n");
+        printf("      MPI Search-mode PSRFITs Combiner\n");
+        printf("              by Scott M. Ransom\n\n");
+    }
+
+    // Determine the hostnames of the processes
     {
         FILE *hostfile;
         
@@ -41,36 +57,9 @@ int main(int argc, char *argv[])
             fflush(NULL);
         }
     }
-
-    gpuids = (int *)malloc(sizeof(int), numbands);
-    for (ii = 1 ; ii <= numbands ; ii++) gpuids[ii] = ii;
-
-    // Extract the original group handle
-    MPI_Comm_group(MPI_COMM_WORLD, &all_group); 
-    // Add the GPU nodes to the new group
-    MPI_Group_incl(all_group, numbands, gpuids, &gpu_group);
-    free(gpuids);
-
-    // Create new communicator
-
-    MPI_Comm_create(MPI_COMM_WORLD, gpu_group, &gpu_comm); 
-    MPI_Group_rank(gpu_group, &gpuid); 
-
-
-    // Call usage() if we have no command line arguments
-    if (argc == 1) {
-        if (myid == 0) {
-            printf("usage:  mpimerge_psrfits basefilename\n\n");
-        }
-        MPI_Finalize();
-        exit(1);
-    }
     
-    if (myid == 0) { // Master proc only
-        printf("\n\n");
-        printf("      MPI Search-mode PSRFITs Combiner\n");
-        printf("              by Scott M. Ransom\n\n");
-    } else { // all other procs
+    // Basefilenames for the GPU nodes
+    if (myid > 0) {
         sprintf(pf.basefilename, "/data/gpu/partial/%s/%s", 
                 hostname, argv[1]);
     }
@@ -90,7 +79,7 @@ int main(int argc, char *argv[])
         fread(&hdr, 1, HDRLEN, psrfitsfile);
         fclose(psrfitsfile);
 
-        // Send the heafer to the master proc
+        // Send the header to the master proc
         MPI_Send(hdr, HDRLEN, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
 
     } else if (myid == 0) {
@@ -118,6 +107,8 @@ int main(int argc, char *argv[])
         pf.filenum = 0;
         pf.multifile = 1;
         pf.filename[0] = '\0';
+        nc = pf.hdr.nchan;
+        ncnp = pf.hdr.nchan * pf.hdr.npol;
         pf.hdr.orig_nchan *= numbands;
         pf.hdr.nchan *= numbands;
         pf.hdr.fctr = pf.hdr.fctr - 0.5 * pf.hdr.BW + numbands/2.0 * pf.hdr.BW;
@@ -127,45 +118,119 @@ int main(int argc, char *argv[])
     }
 
     // Open the input PSRFITs files for real
-    if (myid > 0) status = psrfits_open(&pf);
+    if (myid > 0) {
+        status = psrfits_open(&pf);
+        nc = pf.hdr.nchan;
+        ncnp = pf.hdr.nchan * pf.hdr.npol;
+    }
 
     // Alloc data buffers for the PSRFITS files
     pf.sub.dat_freqs = (float *)malloc(sizeof(float) * pf.hdr.nchan);
     pf.sub.dat_weights = (float *)malloc(sizeof(float) * pf.hdr.nchan);
-    pf.sub.dat_offsets = (float *)malloc(sizeof(float)
-                                         * pf.hdr.nchan * pf.hdr.npol);
-    pf.sub.dat_scales  = (float *)malloc(sizeof(float)
-                                         * pf.hdr.nchan * pf.hdr.npol);
+    pf.sub.dat_offsets = (float *)malloc(sizeof(float) * 
+                                         pf.hdr.nchan * pf.hdr.npol);
+    pf.sub.dat_scales  = (float *)malloc(sizeof(float) * 
+                                         pf.hdr.nchan * pf.hdr.npol);
     pf.sub.data = (unsigned char *)malloc(pf.sub.bytes_per_subint);
 
-    // Now loop over the rows...
-    MPI_Barrier(MPI_COMM_WORLD);
-    if (myid > 0) {
-        do {
-            status = psrfits_read_subint(&pf);
-            // Combine statuses of GPU nodes by summing....
-            MPI_Allreduce(&status, &statsum, 1, MPI_INT, MPI_SUM, gpu_comm); 
-            if (statsum) {
-                // send statsum to master
-                break;
-            }
+    // Counts and offsets for MPI_Gatherv
+    counts = (int *)malloc(sizeof(int) * numprocs);
+    offsets = (int *)malloc(sizeof(int) * numprocs);
+    counts[0] = offsets[0] = 0;  //  master sends nothing
+
+    // Now loop over the rows (i.e. subints)...
+    do {
+        MPI_Barrier(MPI_COMM_WORLD);
+        // Read the current subint from each of the "slave" nodes
+        if (myid > 0) status = psrfits_read_subint(&pf);
+        // Combine statuses of all nodes by summing....
+        MPI_Allreduce(&status, &statsum, 1, 
+                      MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+        if (statsum) break;
             
-            // Step over all the spectra * polns
-            for (jj = 0 ; jj < pf.hdr.nsblk * pf.hdr.npol ; jj++) {
-            }
+        if (myid == 1) { // Send all of the non-band-specific parts to master
+            MPI_Send(&pf.sub.tsubint, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+            MPI_Send(&pf.sub.offs, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+            MPI_Send(&pf.sub.lst, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+            MPI_Send(&pf.sub.ra, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+            MPI_Send(&pf.sub.dec, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+            MPI_Send(&pf.sub.glon, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+            MPI_Send(&pf.sub.glat, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+            MPI_Send(&pf.sub.feed_ang, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+            MPI_Send(&pf.sub.pos_ang, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+            MPI_Send(&pf.sub.par_ang, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+            MPI_Send(&pf.sub.tel_az, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+            MPI_Send(&pf.sub.tel_zen, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+        } else if (myid == 0) { // Receive all of the non-data parts
+            MPI_Recv(&pf.sub.tsubint, 1, MPI_DOUBLE, 1, 0, MPI_COMM_WORLD, &mpistat);
+            MPI_Recv(&pf.sub.offs, 1, MPI_DOUBLE, 1, 0, MPI_COMM_WORLD, &mpistat);
+            MPI_Recv(&pf.sub.lst, 1, MPI_DOUBLE, 1, 0, MPI_COMM_WORLD, &mpistat);
+            MPI_Recv(&pf.sub.ra, 1, MPI_DOUBLE, 1, 0, MPI_COMM_WORLD, &mpistat);
+            MPI_Recv(&pf.sub.dec, 1, MPI_DOUBLE, 1, 0, MPI_COMM_WORLD, &mpistat);
+            MPI_Recv(&pf.sub.glon, 1, MPI_DOUBLE, 1, 0, MPI_COMM_WORLD, &mpistat);
+            MPI_Recv(&pf.sub.glat, 1, MPI_DOUBLE, 1, 0, MPI_COMM_WORLD, &mpistat);
+            MPI_Recv(&pf.sub.feed_ang, 1, MPI_DOUBLE, 1, 0, MPI_COMM_WORLD, &mpistat);
+            MPI_Recv(&pf.sub.pos_ang, 1, MPI_DOUBLE, 1, 0, MPI_COMM_WORLD, &mpistat);
+            MPI_Recv(&pf.sub.par_ang, 1, MPI_DOUBLE, 1, 0, MPI_COMM_WORLD, &mpistat);
+            MPI_Recv(&pf.sub.tel_az, 1, MPI_DOUBLE, 1, 0, MPI_COMM_WORLD, &mpistat);
+            MPI_Recv(&pf.sub.tel_zen, 1, MPI_DOUBLE, 1, 0, MPI_COMM_WORLD, &mpistat);
+        }
+
+        // Now gather the vector quantities...
+
+        // Vectors of length nchan
+        for (ii = 1 ; ii < numprocs ; ii++) {
+            counts[ii] = nc;
+            offsets[ii] = (ii - 1) * nc;
+        }
+        status = MPI_Gatherv(pf.sub.dat_freqs, nc, MPI_FLOAT, 
+                             pf.sub.dat_freqs, counts, offsets, MPI_FLOAT, 
+                             0, MPI_COMM_WORLD);
+        status = MPI_Gatherv(pf.sub.dat_weights, nc, MPI_FLOAT, 
+                             pf.sub.dat_weights, counts, offsets, MPI_FLOAT, 
+                             0, MPI_COMM_WORLD);
+        
+        // Vectors of length nchan * npol
+        for (ii = 1 ; ii < numprocs ; ii++) {
+            counts[ii] = ncnp;
+            offsets[ii] = (ii - 1) * ncnp;
+        }
+        status = MPI_Gatherv(pf.sub.dat_offsets, ncnp, MPI_FLOAT, 
+                             pf.sub.dat_offsets, counts, offsets, MPI_FLOAT, 
+                             0, MPI_COMM_WORLD);
+        status = MPI_Gatherv(pf.sub.dat_scales, ncnp, MPI_FLOAT, 
+                             pf.sub.dat_scales, counts, offsets, MPI_FLOAT, 
+                             0, MPI_COMM_WORLD);
+
+        // Vectors of length nchan for the raw data
+        for (ii = 1 ; ii < numprocs ; ii++) {
+            counts[ii] = nc;
+            offsets[ii] = (ii - 1) * nc;
+        }
+        // Step over all the spectra * polns (Note: this does a "corner turn)
+        for (ii = 0 ; ii < pf.hdr.nsblk * pf.hdr.npol ; ii++) {
+            status = MPI_Gatherv(pf.sub.data + ii * nc, nc, MPI_UNSIGNED_CHAR, 
+                                 pf.sub.data + ii * nc * numbands, 
+                                 counts, offsets, MPI_UNSIGNED_CHAR, 
+                                 0, MPI_COMM_WORLD);
+        }
             
-        } while (statsum == 0);
-    } else { // master
-        do {
-            // Write the new row to the output file
+        // Write the new row to the output file
+        if (myid == 0)
             status = psrfits_write_subint(&pf);
-            if (status) break;
-        } while (statsum == 0);
-    }
 
+    } while (statsum == 0);
 
-    // Loop over each spectra and re-arrange them so they are a single band
-
+    // Free the arrays
+    free(pf.sub.dat_freqs);
+    free(pf.sub.dat_weights);
+    free(pf.sub.dat_offsets);
+    free(pf.sub.dat_scales);
+    free(pf.sub.data);
+    free(counts);
+    free(offsets);
+    
+    // Close the files and finalize things
     status = psrfits_close(&pf);
     MPI_Finalize();
     exit(0);
