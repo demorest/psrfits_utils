731ad1520a8616faabd8e58b8ecf59009b7338ce
Paul Demorest <pdemores@beef.(none)>
2010-05-09 21:47:30 -0400

    Added -U (unsigned) option to psrfits_singlepulse.

--- psrfits_singlepulse.c
+++ psrfits_singlepulse.c
@@ -35,6 +35,7 @@ void usage() {
             "  -F nn, --foldfreq=nn     Fold at constant freq (Hz)\n"
             "  -C, --cal                Cal folding mode\n"
             "  -u, --unsigned           Raw data is unsigned\n"
+            "  -U n, --nunsigned        Num of unsigned polns\n"
             "  -q, --quiet              No progress indicator\n"
           );
 }
@@ -57,6 +58,7 @@ int main(int argc, char *argv[]) {
         {"foldfreq",1, NULL, 'F'},
         {"cal",     0, NULL, 'C'},
         {"unsigned",0, NULL, 'u'},
+        {"nunsigned",1, NULL, 'U'},
         {"quiet",   0, NULL, 'q'},
         {"help",    0, NULL, 'h'},
         {0,0,0,0}
@@ -71,7 +73,7 @@ int main(int argc, char *argv[]) {
     char polyco_file[256] = "";
     char par_file[256] = "";
     char source[24];  source[0]='\0';
-    while ((opt=getopt_long(argc,argv,"o:n:b:j:i:f:T:L:s:p:P:F:Cuqh",long_opts,&opti))!=-1) {
+    while ((opt=getopt_long(argc,argv,"o:n:b:j:i:f:T:L:s:p:P:F:CuU:qh",long_opts,&opti))!=-1) {
         switch (opt) {
             case 'o':
                 strncpy(output_base, optarg, 255);
@@ -121,6 +123,9 @@ int main(int argc, char *argv[]) {
                 break;
             case 'u':
                 raw_signed=0;
+                break;
+            case 'U':
+                raw_signed = 4 - atoi(optarg);
                 break;
             case 'q':
                 quiet=1;
